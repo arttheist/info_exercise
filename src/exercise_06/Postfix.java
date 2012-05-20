9743c699fade03ef78727e082812e15e8c996bf8
Till Leinen <till.leinen@gmx.de>
2012-05-20 16:13:33 +0200

    Implemented Node, LinkedList, Stack, Underflow Exception and changed a few things in Postfix

--- src/exercise_06/Postfix.java
+++ src/exercise_06/Postfix.java
@@ -5,31 +5,37 @@ public class Postfix {
 	// A Method that takes a String representing a postfix expression and determines the value.
 	public int evaluate (String pfx){
 		
-		Stack<String> stack = new Stack();
+		Stack<String> stack = new Stack<String>();
+		int result = 0;
 
 	    //While pfx is not empty:
-		while(pfx.length() == 0) {
+		while(pfx.length() != 0) {
 
 			int expressionResult;
-			int leftHand;
-			int rightHand;
+			int leftHand = 0;
+			int rightHand = 0;
 			
 			//nextToken = next token of pfx.
-			String nextToken = pfx;
+			String nextToken = pfx.substring(0,1);
 			
 			//If nextToken is an operand, push it;
 			if (nextToken.matches("[0-9]")) {
-				Stack.push(nextToken);
+				stack.push(nextToken);
 			}
 			
 	        //If nextToken is an operator:
 			if (nextToken.matches("[+,*/]")) {
 				
-				// put the top into rightHand, pop it;
-				rightHand = Integer.parseInt(Stack.pop());
-	            
-	            //put the top into leftHand, pop it;
-				leftHand = Integer.parseInt(Stack.pop());
+				try {
+					// put the top into rightHand, pop it;
+					rightHand = Integer.parseInt(stack.pop());
+			        
+			        //put the top into leftHand, pop it;
+					leftHand = Integer.parseInt(stack.pop());
+				} catch ( StackUnderflow e ) {
+					System.out.println("The String is not a valid Postfix-Notation");
+					e.printStackTrace();
+				}
 				
 				char operator = nextToken.charAt(0);
 				
@@ -37,11 +43,16 @@ public class Postfix {
 				expressionResult = leftHand + operator + rightHand;
 	            
 	            //push the result
-				Stack.push(Integer.toString(expressionResult));
+				stack.push(Integer.toString(expressionResult));
 			}
 		}
 		
 		//The top of the stack is the result.
-		return Stack.pop();
+		try {
+			result = Integer.parseInt(stack.pop());
+		} catch (StackUnderflow e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
