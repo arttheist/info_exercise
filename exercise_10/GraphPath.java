7035559d38604cbfe0c746eb9ce139561cc947e6
Till Leinen <till.leinen@gmx.de>
2012-06-11 21:45:56 +0200

    Added cheapestPath algorithm

--- exercise_10/GraphPath.java
+++ exercise_10/GraphPath.java
@@ -7,9 +7,21 @@ public class GraphPath {
 	ArrayList<Vertex> vertexList = new ArrayList<Vertex>();
 	ArrayList<Integer> weightList = new ArrayList<Integer>();
 	
-	public GraphPath() {
-		
+	public GraphPath(Vertex v) {
+		vertexList.add(v);
 	}
 	
+	public void addVertex(Vertex v, int w) {
+		vertexList.add(v);
+		weightList.add(w);
+	}
 	
+	public String pathToString() {
+		String result = "";
+		for(int i = 0; i < weightList.size(); i++) {
+			result += vertexList.get(i).getName() + " --" + weightList.get(i) + "-- ";
+		}
+		result += vertexList.get(vertexList.size() - 1);
+		return result;
+	}
 }
--- exercise_10/WeightedGraph.java
+++ exercise_10/WeightedGraph.java
@@ -51,4 +51,73 @@ public class WeightedGraph {
 	public ArrayList<Vertex> getAllVertices() {
 		return vertexList;
 	}
+	
+	public GraphPath getCheapestPath(String vertexName1, String vertexName2) throws VertexNotFoundException, EdgeNotFoundException {
+		// Set distance of all vertices to infinity (-1)
+		int[] distances = new int[vertexList.size()];
+		for(int i : distances)
+			distances[i] = -1;
+		
+		//Set distance of start vertex to 0
+		distances[vertexList.indexOf(getVertexByName(vertexName1))] = 0;
+		
+		//store previousVertices, if Vertex is visited and how much vertices are visited
+		Vertex[] previousVertices = new Vertex[vertexList.size()];
+		boolean[] visited = new boolean[vertexList.size()];
+		int visitedVertices = vertexList.size();
+		
+		//While unvisited Vertices left
+		while(visitedVertices > 0) {
+			//get vertex with minimal distance
+			Vertex nearestVertex = new Vertex("default");
+			int nearestDistance = -1;
+			for(Vertex v : vertexList)
+				if(distances[vertexList.indexOf(v)] < nearestDistance || nearestDistance == -1) {
+					nearestVertex = v;
+					nearestDistance = distances[vertexList.indexOf(v)];
+				}
+			Vertex v = nearestVertex;
+			//set v to visited
+			visited[vertexList.indexOf(v)] = true;
+			visitedVertices--;
+			
+			//get all unvisited neighbours
+			ArrayList<Vertex> unvisitedNeighbours = new ArrayList<Vertex>();
+			for(Vertex n : getNeighbours(v))
+				if(!visited[vertexList.indexOf(n)])
+						unvisitedNeighbours.add(n);
+			for(Vertex n : unvisitedNeighbours) {
+				// distance of neighbour
+				int d = distances[vertexList.indexOf(n)];
+				// distance between n and v
+				int dBetween = getEdgeByVertices(v.getName(), n.getName()).getWeight();
+				// if distance of the neighbour is greater than distance of v plus weight of the edge between
+				if(d == -1 || d > distances[vertexList.indexOf(v)] + dBetween) {
+					// set distance of neighbour to distance of v plus weight of the edge between
+					distances[vertexList.indexOf(n)] = distances[vertexList.indexOf(v)] + dBetween;
+					// set neighbours previous Vertex to v
+					previousVertices[vertexList.indexOf(n)] = v;
+				}
+			}
+		}
+		GraphPath cheapestPath = new GraphPath(getVertexByName(vertexName2));
+		Vertex v = getVertexByName(vertexName2);
+		while(previousVertices[vertexList.indexOf(v)] != null) {
+			cheapestPath.addVertex(previousVertices[vertexList.indexOf(v)], getWeight(v.getName(), previousVertices[vertexList.indexOf(v)].getName()));
+			v = previousVertices[vertexList.indexOf(v)];
+		}
+		return cheapestPath;
+	}
+	
+	private ArrayList<Vertex> getNeighbours(Vertex v) {
+		ArrayList<Vertex> neighbours = new ArrayList<Vertex>();
+		for(Edge e : edgeList) {
+			if(e.getFirst() == v)
+				neighbours.add(e.getSecond());
+			else
+				neighbours.add(e.getFirst());
+		}
+		return neighbours;
+	}
+	
 }
