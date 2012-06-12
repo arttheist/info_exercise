e8231e7caa5eb5aa711488e57047c2aff4b9e6e8
Till Leinen <till.leinen@gmx.de>
2012-06-12 12:33:33 +0200

    arhrahb

--- exercise_10/RandomGraph.java
+++ exercise_10/RandomGraph.java
@@ -1,49 +1,22 @@
 package exercise_10;
 
-import java.util.*;
-
 public class RandomGraph {
 	
-	WeightedGraph mightyGraph = new WeightedGraph();
+	private WeightedGraph mightyGraph;
 	
-	
-
 	public RandomGraph(int numberOfVertex, int numberOfEdges) {
 		
-		char numbers1 = 'A';
-		char numbers2 = 'A';
-		
 		for (int i=0;i<numberOfVertex;i++) {
-			
-			String vertexName = Character.toString(numbers1)+Character.toString(numbers2);
-			mightyGraph.addVertex(vertexName);
-			numbers2++;
-		
-			if (numbers2=='Z') {
-				numbers1++;
-				numbers2='A';
-			}
+			char numbers = 'A';
+			String vertexNumbers = Character.toString(numbers);
+			mightyGraph.addVertex(vertexNumbers);
+			numbers++;
 		}
-		
-		Random generator = new Random();
 		
 		for (int i = 0; i<numberOfEdges;i++) {
-			int rdm1 = generator.nextInt(mightyGraph.getAllVertices().size());
-			int rdm2 = generator.nextInt(mightyGraph.getAllVertices().size());
 			
-			while (rdm1 == rdm2)
-					rdm2 = generator.nextInt(mightyGraph.getAllVertices().size())+1;
-			
-			String firstVertex = mightyGraph.getAllVertices().get(rdm1).getName();
-			String secondVertex = mightyGraph.getAllVertices().get(rdm2).getName();
-			int randomWeight = generator.nextInt( Integer.MAX_VALUE ) + 1;
-			mightyGraph.addEdge(firstVertex,secondVertex,randomWeight);
 		}
 		
-	}
-	
-	public WeightedGraph getGraph() {
-		return mightyGraph;
 	}
 	
 }
