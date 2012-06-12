a1154c7b7b83fb48c46fc90f4e8de1f576dda2ba
Rene Vos <re.vos@arttheisten.de>
2012-06-12 12:50:35 +0200

    File Reader

--- exercise_11/HashTableDict.java
+++ exercise_11/HashTableDict.java
@@ -1,11 +1,24 @@
 package exercise_11;
 
+
 import java.util.HashMap;
+import java.io.*;
 
 public class HashTableDict {
 
 	public HashTableDict(String filename) {
 		HashMap<int, >
+		
+		String line = null;
+		String content = "";
+		
+		try {	
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+	        while ((line = reader.readLine()) != null) {
+	        	content += line;
+	        }
+		}  catch(IOException e) {
+        	System.err.println("Speichern fehlgeschlagen!");
+        }
 	}
-	
 }
