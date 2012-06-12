c58bb7dbd50d04f1881e064b308d116f10624b72
Till Leinen <till.leinen@gmx.de>
2012-06-12 12:51:04 +0200

    started with basic structure

--- exercise_11/HashTableDict.java
+++ exercise_11/HashTableDict.java
@@ -2,10 +2,16 @@ package exercise_11;
 
 import java.util.HashMap;
 
+import exercise_06.LinkedList;
+
 public class HashTableDict {
 
 	public HashTableDict(String filename) {
-		HashMap<int, >
+		HashMap<Integer, LinkedList<String>> dict = new HashMap<Integer, LinkedList<String>>();
+		while(!fileend) {
+			String s = nextLine;
+			s = s.toLowerCase();
+		}
 	}
 	
 }
