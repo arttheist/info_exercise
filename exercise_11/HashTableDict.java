951ba4a375e32d52619d94b31cbdd356329d918e
Till Leinen <till.leinen@gmx.de>
2012-06-18 18:59:20 +0200

    Finished E11

--- exercise_11/HashTableDict.java
+++ exercise_11/HashTableDict.java
@@ -4,57 +4,93 @@ package exercise_11;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class HashTableDict {
-	private HashMap<Integer, LinkedList<String>> dict;
+	private static HashMap<Integer, LinkedList<String>> dict;
 	
-	public void main(String[] args) {
-		Iterator it = dict.entrySet().iterator();
-		while(it.hasNext())
-			Map.Entry<Integer, LinkedList<String>> pair = it.next();
-			System.out.println();
+	public static void main(String[] args) {
+		dict = new HashMap<Integer, LinkedList<String>>();
+		fill();
+		printStats();
+		BufferedReader stdIn =
+		        new BufferedReader(new InputStreamReader(System.in));
+		    String string;
+		    try {
+				while ((string = stdIn.readLine()) != null) {
+				    LinkedList<String> l = dict.get(calcKey(string));
+				    if(l != null)
+				    	for(String s : l)
+				    		System.out.println(s);
+				    else
+				    	System.out.println("No matches found.");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	}
 
-	public HashTableDict() {
-		dict = new HashMap<Integer, LinkedList<String>>();		
+	private static void printStats() {
+		int longestChain = 0;
+		int entries = 0;
+		for (Entry<Integer, LinkedList<String>> entry : dict.entrySet())
+		{
+			String result = entry.getKey() + "/" + entry.getValue().size() + "/";
+		    for(String s : entry.getValue()) {
+		    	result += s + ", ";
+		    }
+		    System.out.println(result);
+		    if(entry.getValue().size() > longestChain)
+		    	longestChain = entry.getValue().size();
+		    entries++;
+		}
+		System.out.println("Longest Chain:" + longestChain);
+		System.out.println(entries + " entries");
+	}
+	
+	private static int calcKey(String s) {
+		s = s.toLowerCase();
+		char[] content = s.toCharArray();
+		Arrays.sort(content);
+		int i = content.length;
+		double sum = 0;
+		for(char c : content) {
+			char a = 'a';
+			int value = c - a + 1;
+			sum += value*Math.pow(27, i);
+			i--;
+		}
+		sum = sum % 4912;
+		return (int) sum;
+	}
+	
+	public static void fill() {		
 		String line = null;
 		String content = "";
 		
 		try {	
-			BufferedReader reader = new BufferedReader(new FileReader("sevenwords.txt"));
+			BufferedReader reader = new BufferedReader(new FileReader("D:/Till/study/term_02/info2/excercises/Exercises_ReneTill/info_exercise/exercise_11/sevenwords.txt"));
 	        while ((line = reader.readLine()) != null) {
 	        	content += line + " ";
 	        }
 		}  catch(IOException e) {
-        	System.err.println("Speichern fehlgeschlagen!");
+        	System.err.println("Lesen fehlgeschlagen!");
         }
 		
-		String[] words = content.split(" "); 
+		String[] words = content.split(" ");
+
 		
 		for(String s : words) {
-			s = s.toLowerCase();
 			String save = s;
-			int sum = 0;
-			int i = 0;
-			while(s.length() > 0) {
-				char c = s.charAt(s.length() - 1);
-				char a = 'a';
-				int value = c - a + 1;
-				sum += value*Math.pow(26, i);
-				s.substring(0 , s.length() - 1);
-				i++;
-			}
-			sum = sum % 1000;
-			LinkedList<String> values = dict.get(sum);
+			int key = calcKey(s);
+			LinkedList<String> values = dict.get(key);
 			if(values == null) {
 				values = new LinkedList<String>();
 			}
 			values.add(save);
-			dict.put(sum, values);
+			dict.put(key, values);
 		}
 	}
 }
